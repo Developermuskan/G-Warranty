6e40e7b2983e8d9231a5374eed4e54c8aec23598
Developermuskan <vermamuskan919@gmail.com>
2025-11-06 21:58:24 +0530

    Connecting live db

--- db.js
+++ db.js
@@ -7,7 +7,8 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false } // required for Render Postgres
 });
 
 pool.connect()
